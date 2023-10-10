8e7819d516b286b4f693c2cfc4ca2ee23f64d0d3
Alex Li <github@alexv525.com>
2023-10-10 16:58:25 +0800

    Update wasm_path.ts

--- src_ts/wasm_path.ts
+++ src_ts/wasm_path.ts
@@ -1,6 +1,4 @@
-import { URL, fileURLToPath } from "url";
-
-export function path(wasmFilename: string): string {
-  const url = new URL(wasmFilename, import.meta.url);
-  return fileURLToPath(url);
+export function path(wasmFilename) {
+    const path = require('path').join(__dirname, `${wasmFilename}`);
+    return path;
 }
